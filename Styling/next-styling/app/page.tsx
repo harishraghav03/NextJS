d944000b1914383dba143b5fb392719c5ac69a3c
harishraghav03 <harishraghav@yectra.com>
2025-04-07 10:45:00 +0530

    Daisy UI implementation

---
+++ Styling/next-styling/app/page.tsx
@@ -0,0 +1,13 @@
+// import style from "./page.module.css";
+
+export default function Home() {
+  return (
+    // <div className={style.container}>
+    <>
+      <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500">
+        <h1>Home</h1>
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </>
+  );
+}
