b51b11386dbc79ebc3d732312ead841e7d1d9e8a
harishraghav03 <harishraghav@yectra.com>
2025-04-17 15:22:57 +0530

    Custom Error Message

--- Routing/routing/app/users/[id]/page.tsx
+++ Routing/routing/app/users/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -6,6 +7,7 @@ interface Props {
 
 export default async function UsersDetailsPage({ params }: Props) {
   const { id } = await params;
+  if (id > 10) notFound();
 
   // This fetching params will only work in Pages
   return <div>UsersDetailsPage {id}</div>;
