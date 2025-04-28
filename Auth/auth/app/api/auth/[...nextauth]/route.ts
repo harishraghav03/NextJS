23a19a13d709acf5660f6d4f864c036a7233ac43
harishraghav03 <harishraghav@yectra.com>
2025-04-28 15:10:45 +0530

    Implemented the DB adapters

--- Auth/auth/app/api/auth/[...nextauth]/route.ts
+++ Auth/auth/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,25 @@
 // ... -> catch all
 
+import { prisma } from "@/app/lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-import NextAuth from "next-auth";
-
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-}
+  session: {
+    strategy: "jwt",
+  },
+};
+
+// By default, the session strategy is JWT, but when using an adapter, it changes to DB. At this time, we cannot use DB sessions with social logins or OAuth Providers
+// Change the session strategy
 
 const handler = NextAuth(authOptions);
 
