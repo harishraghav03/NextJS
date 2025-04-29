5227791fdc5f0399071bb1c9e426665687acc3db
harishraghav03 <harishraghav@yectra.com>
2025-04-29 10:57:52 +0530

    Sending Response to the client after email send

--- Email/email/app/api/send-email/route.tsx
+++ Email/email/app/api/send-email/route.tsx
@@ -1,13 +1,15 @@
 import WelcomeTemplate from "@/emails/WelcomeTemplate";
+import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
-  resend.emails.send({
+  await resend.emails.send({
     from: "harishraghav@yectra.com",
     to: "example.gmail.com",
     subject: "Hola!",
     react: <WelcomeTemplate name="Harish" />,
   });
+  return NextResponse.json("Email send successfully");
 }
