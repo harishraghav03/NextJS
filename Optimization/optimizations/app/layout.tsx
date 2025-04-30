8181abba7dceccab097c126150826a7d3fa21764
harishraghav03 <harishraghav@yectra.com>
2025-04-30 12:45:27 +0530

    refactor: remove unused font imports for cleaner code

--- Optimization/optimizations/app/layout.tsx
+++ Optimization/optimizations/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Roboto } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
+// import { Roboto } from "next/font/google";
 // import localFont from "next/font/local";
 import "./globals.css";
 import GoogleAnalyticsBody from "./components/GoogleAnalyticsBody";
@@ -15,10 +16,10 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500"],
-});
+// const roboto = Roboto({
+//   subsets: ["latin"],
+//   weight: ["400", "500"],
+// });
 
 // const poppins = localFont({
 //   src: "font_file",
